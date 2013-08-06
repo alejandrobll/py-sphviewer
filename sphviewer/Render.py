2ad02f1f18583c4ce5d289d54a9deba16711d737
bllalejandro@gmail.com <bllalejandro@gmail.com>
2013-08-06 03:45:30 +0000

    Logscale and histogram functions added

--- sphviewer/Render.py
+++ sphviewer/Render.py
@@ -31,6 +31,10 @@ class Render():
         ysize = Scene.Camera.get_params()['ysize']
         self.__image = self.__make_render(x,y,t,mass,xsize,ysize)
 
+        #lets define some flags
+        self.__logscale_flag = False;
+
+
     def __make_render(self,x,y,t,mass,xsize,ysize):
         n=int(len(x))
         image = np.zeros([ysize,xsize],dtype=(np.float32))
@@ -59,9 +63,28 @@ class Render():
     def get_min(self):
         return np.min(self.__image)
 
-    def logscale(self):
-        self.__image = np.log10(self.__image+1)
+    def get_extent():
+        return self.Scene.get_extent()
 
+    def set_logscale(self,t=True):
+        if(t == self.get_logscale()):
+            return
+        else:
+            if(t):
+                self.__image = np.log10(self.__image+1)
+                self.__logscale_flag = True;
+            else:
+                self.__image = 10**self.__image-1.
+                self.__logscale_flag = False;
+
+    def get_logscale(self):
+        return self.__logscale_flag
+
+    def histogram(self,axis=None, **kargs):
+        if(axis == None):
+            axis = plt.gca()
+        axis.hist(self.__image.ravel(), **kargs)
+        
     def save(self,outputfile,**kargs):
         plt.imsave(outputfile, self.__image, **kargs)
 
