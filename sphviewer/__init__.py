a48f98ce93806de02709a47418c86fbfb363e9c9
bllalejandro@gmail.com <bllalejandro@gmail.com>
2013-08-08 18:30:08 +0000

    Agregue funciones para manejar el espacio hsv y las imagenes se devuelven como objetos PIL

--- sphviewer/__init__.py
+++ sphviewer/__init__.py
@@ -6,13 +6,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-
     pos = np.random.rand(3,1000)
     mass = 1.*np.ones(1000)
     hue = np.random.rand(1000)
     sat = np.ones(1000)
     pos = np.random.rand(3,1000)
-    P = Particles(pos[0,:], pos[1,:], pos[2,:], mass, prop1=hue,prop2=sat)
+    P = Particles(pos[0,:], pos[1,:], pos[2,:], mass, prop1=hue,prop2=sat,nb=4) 
     S = Scene(P)
     I = Render.Fancy(S)
     print I.get_min(), I.get_max()
