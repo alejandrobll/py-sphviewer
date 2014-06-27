79437b65b485fbbba8c4d0a055a8ecb9fb0651ee
bllalejandro@gmail.com <bllalejandro@gmail.com>
2014-06-27 02:49:13 +0000

    New setup added. The new version now works using extensions pre-compiled in C

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
+
+module_scene = Extension('sphviewer/extensions/scene', sources = ['sphviewer/extensions/scenemodule.c'],
+                         extra_compile_args=['-fopenmp'],
+                         extra_link_args=['-lgomp'])
+
+module_render = Extension('sphviewer/extensions/render', sources = ['sphviewer/extensions/rendermodule.c'],
+                          extra_compile_args=['-fopenmp'],
+                          extra_link_args=['-lgomp'])
 
 setup(name='py-sphviewer',
-      version='0.88',
+      version='0.156',
       description='Py-SPHViewer is a framework for rendering particles using the smoothed particle hydrodynamics scheme.',
       author='Alejandro Benitez Llambay',
       author_email='alejandrobll@oac.uncor.edu',
       url='https://code.google.com/p/py-sphviewer/',
-      packages=['sphviewer'],
+      packages=['sphviewer','sphviewer.extensions'],
       package_data={'sphviewer': ['*.c','*.txt']},
+      ext_modules = [module_scene, module_render],
       license='GNU GPL v3',
       classifiers=[
         'Programming Language :: Python :: 2.7',
