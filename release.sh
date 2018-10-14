e710a28b8a226db936100a4766b4ec6daa6ec462
alejandro <bllalejandro@gmail.com>
2018-10-14 19:03:05 +0100

    removing files that are not relevant for the webpage

--- release.sh
+++
@@ -1,2 +0,0 @@
-python setup.py register -r pypi
-python setup.py sdist upload -r pypi
--- sphviewer/tools/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-from . import camera_tools
-from . import hsv_tools
-from .QuickView import QuickView
