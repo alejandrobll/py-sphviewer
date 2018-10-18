7cda5d4dfcdadf064c9fdee69766113c25822913
Alejandro Benitez Llambay <alejandro.b.llambay@durham.ac.uk>
2018-10-18 23:04:34 +0100

    fixing bugs of last commit

--- sphviewer/Render.py
+++ sphviewer/Render.py
@@ -54,16 +54,12 @@ class Render(object):
             return
 
         self.Scene = Scene
-        x = Scene._x
-        y = Scene._y
-        t = Scene._hsml
-        kview = Scene._kview
 
-        x,y,t,kview = Scene.get_scene()
         xsize = Scene.Camera.get_params()['xsize']
         ysize = Scene.Camera.get_params()['ysize']
 
-        self.__image = self.__make_render(x,y,t,kview,xsize,ysize)
+        self.__image = self.__make_render(Scene._x,Scene._y,Scene._hsml,
+                                          Scene._kview,xsize,ysize)
         #lets define some flags
         self.__logscale_flag = False;
 
@@ -107,8 +103,7 @@ class Render(object):
         where xsize and ysize are the number of pixels of the image defined by the Camera.
         """
         extent = self.Scene.get_extent()
-        pixel_side = abs(extent[1]-extent[0])
-        return self.__image / pixel_side**2
+        return self.__image
 
     def get_max(self):
         """
